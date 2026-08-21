9615a270db741c7a0eb6b4ba3687881b887c100e
PhanHoang12 <phanhln.23it@vku.udn.vn>
2026-08-21 20:24:33 +0700

    Cập nhật tính năng chỉnh sửa sản phẩm và xóa sản phẩm

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("create/",views.created_product,name="created_product"),
     path("my-product/",views.my_product, name="my_product"),
+    path("update/<int:id>/",views.update_product,name="update_product"),
+    path("delete-product/<int:id>/", views.delete_product, name="delete_product")
 ]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import ProductForm
 import json
 import os
 from django.conf import settings
 from .models import Product
+from django.contrib import messages
+
 
 @login_required
 def created_product(request):
@@ -39,5 +41,83 @@ def my_product(request):
         else:
             product.image_filenames = []
     return render(request,'product/my_product.html',{'products': products})
+@login_required
+def update_product(request,id):
+    product = get_object_or_404(Product, id=id, user = request.user)
+    if product.images:
+        try:
+            old_images = json.loads(product.images)
+        except:
+            old_images = []
+    else:
+        old_images = []
+    if request.method == "GET":
+        form = ProductForm(instance=product)
+        context = {
+            "form": form,
+            "product": product,
+            "old_images": old_images
+        }
+        return render(request, "product/update_product.html",context)
+    form = ProductForm(request.POST, request.FILES, instance=product)
+    if form.is_valid():
+        # Nhận ảnh muốn xóa
+        delete_images = request.POST.getlist("delete_images")
+        # Xóa ảnh khỏi danh sách cũ
+        new_old_images = []
+        for image in old_images:
+            if image not in delete_images:
+                new_old_images.append(image)
+        old_images = new_old_images
+        # Lấy ảnh mới 
+        new_images = request.FILES.getlist("images")
+        total_images = len(old_images)+len(new_images)
+        if total_images > 3:
+            form.add_error("images","Tổng số hình ảnh sau khi cập nhật không được vượt quá 3 ảnh!")
+            context = {
+                "form": form,
+                "product": product,
+                "old_images": old_images
+            }
+            return render(request, "product/update_product.html", context)
+        for image in delete_images:
+            image_path = os.path.join(settings.MEDIA_ROOT, image)
+            if os.path.exists(image_path):
+                os.remove(image_path)
+        for image in new_images:
+            image_path = os.path.join(settings.MEDIA_ROOT,"product", image.name)
+            os.makedirs(
+                os.path.dirname(image_path),
+                exist_ok=True
+            )
+            with open(image_path,"wb+") as destination:
+                for chunk in image.chunks():
+                    destination.write(chunk)
+            old_images.append("product/"+image.name)
+        # reset trước khi lưu
+        old_images = list(old_images)
+        # lưu product
+        product = form.save(commit=False)
+        product.images = json.dumps(old_images)
+        product.save()
+        messages.success(request,"Cập nhật sản phẩm thành công")
+        return redirect("my_product")
+    context ={
+        "form": form,
+        "product": product,
+        "old_images": old_images
+    }
+    return render(request,"product/update_product.html", context)
+@login_required
+def delete_product(request, id):
+    product = get_object_or_404(Product, id=id, user = request.user)
+    # Chỉ cho phép post
+    if request.method == "POST":
+        product.delete()
+        messages.success(request, "Delete product successfully!")
+    return redirect("my_product")
+
+
+    
 
 # Create your views here.
